c70df8ebd5aa4ffde571bd3314deb78e060f5568
Samihan <dharurkarsamihan@gmail.com>
2025-07-24 13:34:56 +0530

    add define scan interval, duration and diff

--- include/ble_scanner.h
+++ include/ble_scanner.h
@@ -5,6 +5,10 @@
 #include <Arduino.h>
 #include <vector>
 
+#define SEND_INTERVAL 10000  // milliseconds (10 seconds)
+#define DIFF 2000            // milliseconds (2 seconds) for connection stability
+#define SCAN_DURATION (SEND_INTERVAL - DIFF) / 1000  // convert to seconds
+
 struct BeaconData
 {
     std::string mac;
